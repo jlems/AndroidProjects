24e857f7c6431dcdc02d3e3a615ecc3646e5ee78
jlems <jlemus@bu.edu>
2016-05-20 23:46:48 -0400

    finished Beer Selector

---
+++ BeerAdviser/app/src/main/java/com/jlemus/beeradviser/BeerExpert.java
@@ -0,0 +1,24 @@
+package com.jlemus.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jlems on 5/20/16.
+ */
+public class BeerExpert {
+
+    List<String> getBrands(String color){
+        List<String> brands = new ArrayList<String>();
+
+        if(color.equals("amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        }
+        else{
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
--- BeerAdviser/app/src/main/java/com/jlemus/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/jlemus/beeradviser/FindBeerActivity.java
@@ -1,13 +1,45 @@
 package com.jlemus.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+import java.util.List;
 
+public class FindBeerActivity extends Activity {
+
+    private BeerExpert expert = new BeerExpert();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        String beerType = String.valueOf(color.getSelectedItem());
+        //brands.setText(beerType);
+
+        List<String> beerTypeList = expert.getBrands(beerType);
+
+        StringBuilder brandsFormatted = new StringBuilder();
+
+
+
+        for(String x : beerTypeList){
+            brandsFormatted.append(x + "\n");
+            //brands.append(x + "\n");
+            //brands.setText(x);
+        }
+
+        brands.setText(brandsFormatted);
+
+
+
+
     }
 }
